6a3993716ba0e58947784eeffb89b09bd7784615
valyanskiy <valyanskiyandrey@gmail.com>
2025-10-17 22:34:30 +0300

    New design

--- frontend/ac-frontend/src/consultant/ChatView.tsx
+++ frontend/ac-frontend/src/consultant/ChatView.tsx
@@ -76,8 +76,13 @@ export default function ChatView() {
 
     return (
         <div className="flex flex-col h-full bg-sidebarBackground">
-            <div className="p-6 border-b">
-                <h2 className="font-semibold text-xl truncate">Чат с {chat.userName}</h2>
+            <div className="p-4 pb-0">
+                <h2 className="text-3xl font-semibold truncate">Чат с {chat.userName}</h2>
+            </div>
+
+            <div className="flex flex-row justify-center px-2 mt-2">
+                <div
+                    className="h-1.5 w-full self-center rounded-full bg-white/50 border-2 border-white drop-shadow-lg"></div>
             </div>
 
             <div className="flex-1 p-6 overflow-y-auto flex flex-col space-y-4">
@@ -85,7 +90,7 @@ export default function ChatView() {
                     <div
                         key={message.id}
                         className={`${
-                            message.isFromUser ? 'self-end bg-accentColor rounded-[12px_12px_0_12px]' : 'self-start bg-accentColorLightOpposite shadow-sm rounded-[12px_12px_12px_0]'
+                            message.isFromUser ? 'self-end bg-accentColor/35 border-2 border-accentColor drop-shadow-lg rounded-[12px_12px_0_12px]' : 'self-start bg-accentColorLightOpposite/35 border-2 border-accentColorLightOpposite drop-shadow-lg rounded-[12px_12px_12px_0]'
                         }  px-4 py-3 text-black max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl`}
                         style={{ wordBreak: 'break-word' }}
                     >
@@ -94,15 +99,21 @@ export default function ChatView() {
                 ))}
             </div>
 
-            <div className="p-6 border-t bg-sidebarBackground flex items-center gap-3">
+            <div className="flex flex-row justify-center px-2 mb-2">
+                <div
+                    className="h-1.5 w-full self-center rounded-full bg-white/50 border-2 border-white drop-shadow-lg"></div>
+            </div>
+
+
+            <div className="p-2 pb-4  bg-sidebarBackground flex items-center gap-3">
                 <label className="flex-1">
                     <input
                         type="text"
                         placeholder="Введите сообщение..."
-                        className="w-full border rounded-xl px-4 py-3 focus:outline-none"
+                        className="w-full hover:bg-background bg-white/50 border-2 border-white drop-shadow-lg rounded-xl px-4 py-3 focus:outline-none transition-colors duration-300"
                     />
                 </label>
-                <button className="px-6 py-3 rounded-xl bg-accentColor text-black hover:opacity-80 transition-opacity duration-300 font-medium whitespace-nowrap">
+                <button className="px-6 py-3 rounded-xl hover:bg-accentColor bg-accentColor/35 border-2 border-accentColor drop-shadow-lg duration-300 font-medium whitespace-nowrap">
                     Отправить
                 </button>
             </div>
--- frontend/ac-frontend/src/consultant/Consultant.tsx
+++ frontend/ac-frontend/src/consultant/Consultant.tsx
@@ -2,20 +2,27 @@ import { NavLink, Outlet } from "react-router-dom";
 import { testChats } from "./ChatView.tsx";
 
 export default function Consultant() {
-    const linkClass = ({ isActive }: { isActive: boolean }) =>
-        `block p-4 hover:bg-background transition-colors duration-300 rounded-xl cursor-pointer mb-2 ${
-            isActive ? "bg-background" : ""
+    const linkClass = ({isActive}: { isActive: boolean }) =>
+        `transition-colors duration-300 rounded-xl p-3 ${
+            isActive
+                ? "hover:bg-accentColor bg-accentColor/35 border-2 border-accentColor drop-shadow-lg"
+                : "hover:bg-background bg-sidebarBackground/35 border-2 border-sidebarBackground/50 drop-shadow-lg"
         }`;
 
     return (
         <div className="flex flex-row h-screen bg-background text-black">
             {/* Список чатов */}
-            <div className="w-80 min-w-80 max-w-96 bg-sidebarBackground flex flex-col border-r">
-                <div className="p-6 border-b">
-                    <h2 className="text-xl font-semibold">Чаты</h2>
+            <div className="w-80 min-w-80 max-w-96 bg-sidebarBackground flex flex-col">
+                <div className="p-4 pb-0">
+                    <h2 className="text-3xl font-semibold">Чаты</h2>
                 </div>
 
-                <div className="overflow-y-auto flex-1 p-2">
+                <div className="flex flex-row justify-center px-2 my-2">
+                    <div
+                        className="h-1.5 w-full self-center rounded-full bg-white/50 border-2 border-white drop-shadow-lg"></div>
+                </div>
+
+                <div className="overflow-y-auto w-full p-2 flex flex-col gap-4 drop-shadow-2xl h-full">
                     {testChats.map(chat => (
                         <NavLink
                             key={chat.id}
